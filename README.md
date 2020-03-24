d7e59a0ff8441a152940b4c2e4a87fde1f6cbec0
kimseokjung <60689918+kimseokjung@users.noreply.github.com>
2020-03-24 19:11:59 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 #WETUBE
 
 Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
+
+- Home
+- Join
+- Login
+- Search
+- User Detail
+- Edit Profile
+- Change Password
+- Update
+- Video Detail
+- Edit Video
--- init.js
+++ init.js
@@ -1,10 +1,8 @@
-const express = require("express");
-const app = express();
+import app from "./app";
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
-}
 
 app.listen(PORT, handleListening);
