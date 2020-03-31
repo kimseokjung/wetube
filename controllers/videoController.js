b37f0b787f5892be4c956c2a59a2cbdbcc07ac45
kimseokjung <60689918+kimseokjung@users.noreply.github.com>
2020-03-31 16:58:12 +0900

    initial commit

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,6 @@
 import { videos } from "../db";
+import routes from "../routes";
+
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos });
 };
@@ -6,12 +8,18 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req;
-  res.render("search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
-/*export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "Videos" }); */
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }
+  } = req;
+  res.redirect(routes.videoDetail(1231));
+};
+// To Do: Upload and save vidoes
+
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
--- db.js
+++ db.js
@@ -1,10 +1,11 @@
-export const vidoes = [
+export const videos = [
   {
     id: 1231,
     title: "Video 1",
     description: "This is something i love.",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 1231231,
       name: "kim",
@@ -16,7 +17,8 @@ export const vidoes = [
     title: "Video 2",
     description: "This is something i love.",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 1231231,
       name: "kim",
@@ -28,7 +30,8 @@ export const vidoes = [
     title: "Video 3",
     description: "This is something i love.",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 1231231,
       name: "kim",
@@ -40,7 +43,8 @@ export const vidoes = [
     title: "Video 4",
     description: "This is something i love.",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 1231231,
       name: "kim",
